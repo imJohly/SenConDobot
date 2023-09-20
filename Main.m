9bb4db9d6841558af58bfaf68b5c18832ea9297c
Korpie15/RS1 <aidankorpar@gmail.com>
2023-09-20 21:25:11 +1000

    Created Main

---
+++ Main.m
@@ -0,0 +1,81 @@
+clc
+clf
+clear all
+hold on
+
+mdl_dobot
+STEPS = 50;
+
+
+q = zeros(1,5);
+
+pickPosition = transl(0.1, -0.2 , 0.2)
+offset = transl(0,0,0.05) * rpy2tr(0,0,pi/2);
+
+plot3(pickPosition(1,4),pickPosition(2,4),pickPosition(3,4),'r.');
+
+bluePlace = transl(-0.1,0.25,0);
+
+
+floor = surf([-0.35,-0.35;0.45,0.45] ,[-0.45,0.45;-0.45,0.45] ,[0,0;0,0] ...
+,'CData',imread('Floor.jpg') ,'FaceColor','texturemap');
+
+rotate(floor,[0,0,1],180);
+
+%% My Dobot
+realQ = [0,pi/4,pi/4,0,0];
+Dobot.base = transl(0,0,0) * rpy2tr(0,0,0);
+Dobot.plot(q,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','nobase','noshadow');
+lighting none;
+axis([-0.5,0.5,-0.5,0.5,-0.2,0.5])
+
+q = Dobot.getpos;
+q = Dobot.ikcon(pickPosition ,Dobot.getpos);
+Qmatrix = jtraj(Dobot.getpos,q,STEPS);
+for i = 1:STEPS
+
+    Dobot.animate(Qmatrix(i,:));
+    %pause(0.05)
+end
+
+VolumePC(Dobot)
+% q = Dobot.ikcon(bluePlace,Dobot.getpos);
+% Qmatrix = jtraj(Dobot.getpos,q,STEPS);
+% for i = 1:STEPS
+% 
+%     Dobot.animate(Qmatrix(i,:));
+%     %pause(0.05)
+% end
+
+%% UTS Dobot
+% r = DobotMagician();
+% r.model.plot(realQ);
+% r.model.base = transl(0,0,0);
+% r.model.animate(q);
+% 
+% for i = 1:STEPS
+% 
+%     r.model.animate(Qmatrix(i,:));
+%     pause(0.05)
+% end
+
+
+
+
+% Clears plot for somereason
+%vol = VolumePC(r.model);
+
+
+%% Functionsrobot
+
+
+function [matrix] = GetIK(robot,position,STEPS,guess,testpoint)
+
+    Q = robot.getpos();
+    Q = robot.ikcon(position,guess);
+    matrix = jtraj(robot.getpos,Q,STEPS);
+if nargin > 4
+      TestPoint(robot,position)
+
+end
+end
--- myDobotFunctions/AidanVolume.m
+++ myDobotFunctions/AidanVolume.m
@@ -1,71 +1,3 @@
-clc
-clf
-clear all
-hold on
-
-mdl_dobot
-STEPS = 50;
-
-
-q = zeros(1,5);
-
-pickPosition = transl(0.1, -0.2 , 0.2)
-offset = transl(0,0,0.05) * rpy2tr(0,0,pi/2);
-
-plot3(pickPosition(1,4),pickPosition(2,4),pickPosition(3,4),'r.');
-
-bluePlace = transl(-0.1,0.25,0);
-
-surf([-1.7,-1.7;1.8,1.8] ,[-1.8,2.5;-1.8,2.5] ,[0,-1.0;-1.0,-1.0] ...
-,'CData',imread('Floor.jpg') ,'FaceColor','texturemap');
-
-
-
-%% My Dobot
-realQ = [0,pi/4,pi/4,0,0];
-Dobot.base = transl(0,0,0) * rpy2tr(0,0,0);
-Dobot.plot(q,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','nobase','noshadow');
-
-axis([-0.5,0.5,-0.5,0.5,-0.2,0.5])
-
-q = Dobot.getpos;
-q = Dobot.ikcon(pickPosition ,Dobot.getpos);
-Qmatrix = jtraj(Dobot.getpos,q,STEPS);
-for i = 1:STEPS
-
-    Dobot.animate(Qmatrix(i,:));
-    %pause(0.05)
-end
-VolumePC(Dobot)
-% q = Dobot.ikcon(bluePlace,Dobot.getpos);
-% Qmatrix = jtraj(Dobot.getpos,q,STEPS);
-% for i = 1:STEPS
-% 
-%     Dobot.animate(Qmatrix(i,:));
-%     %pause(0.05)
-% end
-
-%% UTS Dobot
-% r = DobotMagician();
-% r.model.plot(realQ);
-% r.model.base = transl(0,0,0);
-% r.model.animate(q);
-% 
-% for i = 1:STEPS
-% 
-%     r.model.animate(Qmatrix(i,:));
-%     pause(0.05)
-% end
-
-
-
-
-% Clears plot for somereason
-%vol = VolumePC(r.model);
-
-
-%% Functionsrobot
-
 function [volume] = VolumePC(robot)
     %profile clear;
     %profile on;
@@ -131,16 +63,4 @@ function [volume] = VolumePC(robot)
 
     %profile off;
     %profile viewer;
-end
-
-
-function [matrix] = GetIK(robot,position,STEPS,guess,testpoint)
-
-    Q = robot.getpos();
-    Q = robot.ikcon(position,guess);
-    matrix = jtraj(robot.getpos,Q,STEPS);
-if nargin > 4
-      TestPoint(robot,position)
-
-end
 end
