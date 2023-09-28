6fb9528760856de7fdf67342c4a65d0e716cd0b5
Korpie15/RS1 <aidankorpar@gmail.com>
2023-09-28 19:19:28 +1000

    Comment out plot in test point

--- myDobotFunctions/TestPoint.m
+++ myDobotFunctions/TestPoint.m
@@ -2,7 +2,7 @@ function isInside = TestPoint(pointCloud, queryPoint)
 
     alpha = 0.06;
     shp = alphaShape(pointCloud, alpha);
-    plot(shp);
+    %plot(shp);
 
     isInside = inShape(shp,queryPoint(1,4),queryPoint(2,4),queryPoint(3,4))
 
