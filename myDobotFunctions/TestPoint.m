77c9bc733dc5fa61e1b5a5249c22e3cc6499aa2c
Korpie15/RS1 <aidankorpar@gmail.com>
2023-10-03 18:46:51 +1100

    Fixed Test Point Function

--- myDobotFunctions/TestPoint.m
+++ myDobotFunctions/TestPoint.m
@@ -1,12 +1,9 @@
-function isInside = TestPoint(pointCloud, queryPoint)
-
-    alpha = 0.06;
-    shp = alphaShape(pointCloud, alpha);
-    %plot(shp);
+function isInside = TestPoint(pointCloud, queryPoint,shp)
 
     isInside = inShape(shp,queryPoint(1,4),queryPoint(2,4),queryPoint(3,4));
 
     if ~isInside
         fprintf('The query point is outside the range of the robot\n');
     end
+
 end
