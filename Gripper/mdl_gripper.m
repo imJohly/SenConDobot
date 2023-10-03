b8516c993cd4b7109386af1b5c0d6d318791f07a
Korpie15/RS1 <aidankorpar@gmail.com>
2023-10-03 21:06:59 +1100

    Added colour to gripper

--- Gripper/mdl_gripper.m
+++ Gripper/mdl_gripper.m
@@ -13,6 +13,7 @@ Gripper1 = SerialLink([
     'name', 'Griper 1');
 %qz = [0 0 0];
 Gripper1.model3d = 'DOBOT/Gripper1';
+Gripper1.links
 
 %Second gripper
 Gripper2 = SerialLink([
@@ -28,16 +29,22 @@ Gripper2.model3d = 'DOBOT/Gripper2';
 % clf
 % hold on
 % newq = [0,0.5,0];
-% Gripper1.plot3d(newq,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','path','D:\OneDrive - UTS\University\Subjects\Year 3\Sem 2\41014 Sensors and Control\Git\SenConDobot\Gripper\Gripper1' );
-%  Gripper2.plot(newq,'nowrist','notiles', 'noarrow','nojaxes','nobase');
-% Gripper2.plot3d(newq,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','path','D:\OneDrive - UTS\University\Subjects\Year 3\Sem 2\41014 Sensors and Control\Git\SenConDobot\Gripper\Gripper2' );
+% DesiredColour = {'black','black','black','silver'} % Links {1,doent matter,2,3}
+%         Gripper1.plot3d(CLOSE_GRIPPER_Q,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','color',DesiredColour);
+%          Gripper2.plot(newq,'nowrist','notiles', 'noarrow','nojaxes','nobase');
 % 
+%         Gripper2.plot3d(CLOSE_GRIPPER_Q,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','color',DesiredColour);
 % 
-% 
-% RedCube = PlaceObject('RedCube.ply');
-% vertices = get(RedCube,'Vertices');
-% transformedVertices = [vertices,ones(size(vertices,1),1)] * transl(0,0,-0.05)';
-% set(RedCube,'Vertices',transformedVertices(:,1:3));
+% % Gripper1.plot3d(newq,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','path','D:\OneDrive - UTS\University\Subjects\Year 3\Sem 2\41014 Sensors and Control\Git\SenConDobot\Gripper\Gripper1' );
+% %  Gripper2.plot(newq,'nowrist','notiles', 'noarrow','nojaxes','nobase');
+% % Gripper2.plot3d(newq,'delay',0.01,'noname','nowrist','notiles', 'noarrow','nojaxes','path','D:\OneDrive - UTS\University\Subjects\Year 3\Sem 2\41014 Sensors and Control\Git\SenConDobot\Gripper\Gripper2' );
+% % 
+% % 
+% % 
+% % RedCube = PlaceObject('RedCube.ply');
+% % vertices = get(RedCube,'Vertices');
+% % transformedVertices = [vertices,ones(size(vertices,1),1)] * transl(0,0,-0.05)';
+% % set(RedCube,'Vertices',transformedVertices(:,1:3));
 % 
 % newq = [0,0.3,0.3];
 % Gripper1.animate(newq);
