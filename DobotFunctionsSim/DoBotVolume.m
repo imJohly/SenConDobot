3d11d73c5405fa958665c79eae31a680fc36700e
Korpie15 <125727595+Korpie15@users.noreply.github.com>
2023-10-26 10:33:39 +1100

    Update DoBotVolume.m

--- DobotFunctionsSim/DoBotVolume.m
+++ DobotFunctionsSim/DoBotVolume.m
@@ -1,90 +1,92 @@
 function [pointCloud,shp] = DoBotVolume(myRobot,loadingBar,plotPoints,boundryTrue)
-    %profile clear;
-    %profile on;
-    
-    stepDeg = 5;
-    stepRads = deg2rad(stepDeg);
-    qlim = myRobot.model.qlim;
-    % Precalculate Point Cloud Size
-    
-    pointCloudeSize = 7481; %hard coded value as hard to do calculation when the limits are variable
-    %prod(floor((qlim(1:3,2)-qlim(1:3,1))/stepRads + 1))
-    pointCloud = zeros(pointCloudeSize,3);
-    z = myRobot.model.base().t;
-    counter = 1;
-    if loadingBar
+%profile clear;
+%profile on;
+
+stepDeg = 5;
+stepRads = deg2rad(stepDeg);
+qlim = myRobot.model.qlim;
+% Precalculate Point Cloud Size
+
+pointCloudeSize = 7481; %hard coded value as hard to do calculation when the limits are variable
+%prod(floor((qlim(1:3,2)-qlim(1:3,1))/stepRads + 1))
+pointCloud = zeros(pointCloudeSize,3);
+z = myRobot.model.base().t;
+counter = 1;
+if loadingBar
     progressbar = waitbar(0, 'Loading Point Cloud...');
-    end
-    for q1 = qlim(1,1):stepRads:qlim(1,2)
-        for q2 = qlim(2,1):stepRads:qlim(2,2)
-            for q3 = (pi/2 - q2):stepRads:deg2rad(140)
+end
+for q1 = qlim(1,1):stepRads:qlim(1,2)
+    for q2 = qlim(2,1):stepRads:qlim(2,2)
+        for q3 = (pi/2 - q2):stepRads:deg2rad(140)
 
-                        q4 = pi - q2 - q3;
-                        q5 = 0;
-                        q = [q1,q2,q3,q4,q5];
+            q4 = pi - q2 - q3;
+            q5 = 0;
+            q = [q1,q2,q3,q4,q5];
 
-                        %tr = robot.fkineUTS(q);
-                        tr = DobotFkReal(myRobot,q);
-                        
-                        
-                        if tr(3,4) > (z(3) -0.001)
-                            pointCloud(counter,:) = tr(1:3,4)';
-                        
-                        end
+            %tr = robot.fkineUTS(q);
+            tr = DobotFkReal(myRobot,q);
 
-                        %pointCloud(counter,:) = tr(1:3,4)';
 
-                        counter = counter + 1 ;
-
-                        if loadingBar
-                        waitbar(counter / pointCloudeSize, progressbar, sprintf('Loading Point Cloud... %d%%', round(counter / pointCloudeSize * 100)));
-                        end
+            if tr(3,4) > (z(3) -0.001)
+                pointCloud(counter,:) = tr(1:3,4)';
 
             end
+
+            %pointCloud(counter,:) = tr(1:3,4)';
+
+            counter = counter + 1 ;
+
+            if loadingBar
+                waitbar(counter / pointCloudeSize, progressbar, sprintf('Loading Point Cloud... %d%%', round(counter / pointCloudeSize * 100)));
+            end
+
         end
     end
+end
 
-    alpha = 0.06;
-    shp = alphaShape(pointCloud, alpha);
-    workingVolume = volume(shp)
+alpha = 0.06;
+shp = alphaShape(pointCloud, alpha);
 
-    if plotPoints
-        if boundryTrue
-            % k = boundary(pointCloud);
-            % trisurf(k,pointCloud(:,1),pointCloud(:,2),pointCloud(:,3),'Facecolor','red','FaceAlpha',0.1)
+fprintf('DoBot Working Volume = %f\n', volume(shp));
 
-            plot(shp);
+if plotPoints
+    if boundryTrue
+        % k = boundary(pointCloud);
+        % trisurf(k,pointCloud(:,1),pointCloud(:,2),pointCloud(:,3),'Facecolor','red','FaceAlpha',0.1)
 
-        else
-            plot3(pointCloud(:,1),pointCloud(:,2),pointCloud(:,3),'r.');
-        end
+        plot(shp);
+
+    else
+        plot3(pointCloud(:,1),pointCloud(:,2),pointCloud(:,3),'r.');
     end
-    [k,av] = convhull(pointCloud);
-    workSpaceVolume = av
-    counter
+end
+[k,av] = convhull(pointCloud);
+
+fprintf('Work Space Volume = %f\n', av);
+fprintf('Number Of Points = %d\n', counter);
 
 
 
 
-    min_x = min(pointCloud(:, 1));
-    max_x = max(pointCloud(:, 1));
-    min_y = min(pointCloud(:, 2));
-    max_y = max(pointCloud(:, 2));
-    min_z = min(pointCloud(:, 3));
-    max_z = max(pointCloud(:, 3));
-    
-    
+min_x = min(pointCloud(:, 1));
+max_x = max(pointCloud(:, 1));
+min_y = min(pointCloud(:, 2));
+max_y = max(pointCloud(:, 2));
+min_z = min(pointCloud(:, 3));
+max_z = max(pointCloud(:, 3));
 
-    % Display the ranges
-    fprintf('Range in the x-direction: %.2f to %.2f\n', min_x, max_x);%, 'Distance is: ',xRadius);
-    fprintf('Range in the y-direction: %.2f to %.2f\n', min_y, max_y);%, 'Distance is: ',yRadius);
-    fprintf('Range in the z-direction: %.2f to %.2f\n', min_z, max_z);%, 'Distance is: ',zRadius);
-    xDiameter = (max_x - min_x)
-    yDiameter = (max_y - min_y)
-    zDiameter = (max_z - min_z)
-    if loadingBar
+
+
+% Display the ranges
+fprintf('Range in the x-direction: %.2f to %.2f\n', min_x, max_x);%, 'Distance is: ',xRadius);
+fprintf('Range in the y-direction: %.2f to %.2f\n', min_y, max_y);%, 'Distance is: ',yRadius);
+fprintf('Range in the z-direction: %.2f to %.2f\n', min_z, max_z);%, 'Distance is: ',zRadius);
+ xDiameter = (max_x - min_x)
+ yDiameter = (max_y - min_y)
+ zDiameter = (max_z - min_z)
+if loadingBar
     close(progressbar);
-    end
-    %profile off;
-    %profile viewer;
+end
+%profile off;
+%profile viewer;
 end
